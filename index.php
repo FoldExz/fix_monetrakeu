611b56070dad9a5c8c3346fff33c1fc08bfbe613
FoldExz <ihsantazakisanjaya@gmail.com>
2026-04-23 14:41:57 +0700

    Change subfolder name references from fix_monetrakeu to montrackeu

--- index.php
+++ index.php
@@ -2,14 +2,14 @@
 
 /**
  * Root index.php untuk deployment Hostinger subfolder.
- * File ini ada di: public_html/fix_monetrakeu/index.php
+ * File ini ada di: public_html/montrackeu/index.php
  * 
  * Tugasnya: mengarahkan request ke public/index.php
  * tanpa Laravel bingung soal subfolder path.
  */
 
 // Perbaiki SCRIPT_NAME agar Laravel tahu path subfolder yang benar
-$_SERVER['SCRIPT_NAME'] = '/fix_monetrakeu/index.php';
+$_SERVER['SCRIPT_NAME'] = '/montrackeu/index.php';
 
 // Muat index.php asli Laravel dari folder public
 require __DIR__ . '/public/index.php';
